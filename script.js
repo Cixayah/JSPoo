13d3f5182efa9e70ea2e8b279279f6759563d755
Cix <gabrielviniciodacosta@gmail.com>
2023-08-16 14:40:26 -0300

    POO

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ class Pessoa {
         this.idade = idade;
     }
     apresentar() {
-        console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+        return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
     }
 }
-const pessoa1 = new Pessoa("Gabriel", 26);
-const pessoa2 = new Pessoa("Larissa", 23);
-const pessoa3 = new Pessoa("Davi", 16);
 
 class Estudante extends Pessoa {
     constructor(nome, idade, curso) {
@@ -17,17 +14,31 @@ class Estudante extends Pessoa {
         this.curso = curso;
     }
     estudar() {
-        console.log(`${this.nome} está estudando ${this.curso}.`);
+        return `${this.nome} está estudando ${this.curso}.`;
     }
 }
+
+const pessoa1 = new Pessoa("Gabriel", 26);
+const pessoa2 = new Pessoa("Larissa", 23);
+const pessoa3 = new Pessoa("Davi", 16);
+
 const estudante1 = new Estudante(pessoa3.nome, pessoa3.idade, "Técnico em Informática");
-estudante1.apresentar(); // Saída: Olá, meu nome é Gabriel e tenho 26 anos.
-estudante1.estudar(); // Saída: Gabriel está estudando Técnico em Informática.
-console.log(); //Apenas para pular linha
 const estudante2 = new Estudante(pessoa2.nome, pessoa2.idade, "Análise e Desenvolvimento de Sistemas");
-estudante2.apresentar(); // Saída: Olá, meu nome é Larissa e tenho 23 anos.
-estudante2.estudar(); // Saída: Larissa está estudando Análise e Desenvolvimento de Sistemas.
-console.log(); //Apenas para pular linha
 const estudante3 = new Estudante(pessoa3.nome, pessoa3.idade, "no Ensino Médio");
-estudante3.apresentar(); // Saída: Olá, meu nome é Davi e tenho 16 anos.
-estudante3.estudar(); //Saída: Davi está estudando no Ensino Médio.
+
+const container = document.createElement("div");
+container.classList.add("container");
+
+const content = document.createElement("p");
+content.innerText = `${estudante1.apresentar()}\n${estudante1.estudar()}`;
+container.appendChild(content);
+
+const content2 = document.createElement("p");
+content2.innerText = `${estudante2.apresentar()}\n${estudante2.estudar()}`;
+container.appendChild(content2);
+
+const content3 = document.createElement("p");
+content3.innerText = `${estudante3.apresentar()}\n${estudante3.estudar()}`;
+container.appendChild(content3);
+
+document.body.appendChild(container);
