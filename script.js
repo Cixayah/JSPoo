5c63cc212eed95b6b1a1d2619c024efcf61bba7a
Cix <gabrielviniciodacosta@gmail.com>
2023-08-16 14:45:17 -0300

    #

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class Pessoa {
         this.nome = nome;
         this.idade = idade;
     }
+
     apresentar() {
         return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
     }
@@ -13,32 +14,39 @@ class Estudante extends Pessoa {
         super(nome, idade);
         this.curso = curso;
     }
+
     estudar() {
         return `${this.nome} está estudando ${this.curso}.`;
     }
 }
 
-const pessoa1 = new Pessoa("Gabriel", 26);
-const pessoa2 = new Pessoa("Larissa", 23);
-const pessoa3 = new Pessoa("Davi", 16);
+const estudanteForm = document.getElementById("estudante-form");
+const estudanteList = document.getElementById("estudante-list");
 
-const estudante1 = new Estudante(pessoa3.nome, pessoa3.idade, "Técnico em Informática");
-const estudante2 = new Estudante(pessoa2.nome, pessoa2.idade, "Análise e Desenvolvimento de Sistemas");
-const estudante3 = new Estudante(pessoa3.nome, pessoa3.idade, "no Ensino Médio");
+estudanteForm.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-const container = document.createElement("div");
-container.classList.add("container");
+    const nome = document.getElementById("nome").value;
+    const idade = parseInt(document.getElementById("idade").value);
+    const curso = document.getElementById("curso").value;
 
-const content = document.createElement("p");
-content.innerText = `${estudante1.apresentar()}\n${estudante1.estudar()}`;
-container.appendChild(content);
+    const estudante = new Estudante(nome, idade, curso);
+    const estudanteContent = document.createElement("div");
+    estudanteContent.classList.add("estudante");
 
-const content2 = document.createElement("p");
-content2.innerText = `${estudante2.apresentar()}\n${estudante2.estudar()}`;
-container.appendChild(content2);
+    const estudanteText = document.createElement("p");
+    estudanteText.innerText = `${estudante.apresentar()}\n${estudante.estudar()}`;
 
-const content3 = document.createElement("p");
-content3.innerText = `${estudante3.apresentar()}\n${estudante3.estudar()}`;
-container.appendChild(content3);
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remover";
+    removeButton.addEventListener("click", function () {
+        estudanteContent.remove();
+    });
 
-document.body.appendChild(container);
+    estudanteContent.appendChild(estudanteText);
+    estudanteContent.appendChild(removeButton);
+
+    estudanteList.appendChild(estudanteContent);
+
+    estudanteForm.reset();
+});
