668d40b6acb7e91830f841f9eee1b430231c064b
Cix <gabrielviniciodacosta@gmail.com>
2023-08-16 14:54:56 -0300

    JS Poo

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ class Estudante extends Pessoa {
 
 const estudanteForm = document.getElementById("estudante-form");
 const estudanteList = document.getElementById("estudante-list");
+const editarBtn = document.getElementById("editar-btn");
+const cancelarBtn = document.getElementById("cancelar-btn");
+
+let editingStudent = null;
+const estudantes = [];
 
 estudanteForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -30,23 +35,66 @@ estudanteForm.addEventListener("submit", function (e) {
     const idade = parseInt(document.getElementById("idade").value);
     const curso = document.getElementById("curso").value;
 
-    const estudante = new Estudante(nome, idade, curso);
-    const estudanteContent = document.createElement("div");
-    estudanteContent.classList.add("estudante");
-
-    const estudanteText = document.createElement("p");
-    estudanteText.innerText = `${estudante.apresentar()}\n${estudante.estudar()}`;
-
-    const removeButton = document.createElement("button");
-    removeButton.innerText = "Remover";
-    removeButton.addEventListener("click", function () {
-        estudanteContent.remove();
-    });
-
-    estudanteContent.appendChild(estudanteText);
-    estudanteContent.appendChild(removeButton);
-
-    estudanteList.appendChild(estudanteContent);
-
-    estudanteForm.reset();
+    if (editingStudent) {
+        // Atualizar o estudante editado
+        editingStudent.nome = nome;
+        editingStudent.idade = idade;
+        editingStudent.curso = curso;
+        updateStudentList();
+        editingStudent = null;
+        estudanteForm.reset();
+        editarBtn.style.display = "none";
+        cancelarBtn.style.display = "none";
+    } else {
+        // Adicionar um novo estudante
+        const estudante = new Estudante(nome, idade, curso);
+        estudantes.push(estudante);
+        updateStudentList();
+        estudanteForm.reset();
+    }
 });
+
+function updateStudentList() {
+    estudanteList.innerHTML = "";
+
+    estudantes.forEach((estudante, index) => {
+        const estudanteContent = document.createElement("div");
+        estudanteContent.classList.add("estudante");
+
+        const estudanteText = document.createElement("p");
+        estudanteText.innerText = `${estudante.apresentar()}\n${estudante.estudar()}`;
+
+        const editButton = document.createElement("button");
+        editButton.innerText = "Editar";
+        editButton.addEventListener("click", function () {
+            document.getElementById("nome").value = estudante.nome;
+            document.getElementById("idade").value = estudante.idade;
+            document.getElementById("curso").value = estudante.curso;
+            editarBtn.style.display = "inline-block";
+            cancelarBtn.style.display = "inline-block";
+            editingStudent = estudante;
+        });
+
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "Remover";
+        removeButton.addEventListener("click", function () {
+            estudantes.splice(index, 1);
+            updateStudentList();
+        });
+
+        estudanteContent.appendChild(estudanteText);
+        estudanteContent.appendChild(editButton);
+        estudanteContent.appendChild(removeButton);
+
+        estudanteList.appendChild(estudanteContent);
+    });
+}
+
+cancelarBtn.addEventListener("click", function () {
+    estudanteForm.reset();
+    editarBtn.style.display = "none";
+    cancelarBtn.style.display = "none";
+    editingStudent = null;
+});
+
+updateStudentList();
